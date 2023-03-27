574020eaa1f4b7e58f3b126725ea4ec9f8cb4772
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-03-27 11:19:29 -0400

    filled out basic info

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () => {
+
+
+    return(
+        <p></p>
+    )
+}
+
+export default Header
---
+++ src/components/ShowInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShowInfo = () => {
+
+
+
+    return(
+        <p></p>
+    )
+}
+
+export default ShowInfo
