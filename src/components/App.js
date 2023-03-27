9936e8519db2b7d77429f485f7d61c4a175ab45e
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-03-27 15:51:32 -0400

    made home work

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import Home from "./Home";
 import SearchResults from "./SearchResults";
@@ -8,12 +8,26 @@ import ShowInfo from "./ShowInfo";
 
 
 function App() {
+
+  const [genreShows, setGenreShows] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/shows")
+      .then((response) => response.json())
+      .then((data) => {
+        setGenreShows(data)
+        
+      });
+  }, []);
+
+  console.log(genreShows)
+
   return (
     <div className="App">
 
       <Header />
       <Routes >
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home genreShows={genreShows} />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path="/show/" element={<ShowInfo />} />
       </Routes>
