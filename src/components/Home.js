a590500ca097da23be4da72ce3f4b891a39fc68a
Seyfican Yalcindag <+>
2023-03-27 21:49:19 +0300

    edit db.json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,41 @@
-import React from "react";
+// import React from "react";
+
+// const Home = () => {
+
+
+//     return(
+//         <p></p>
+//     )
+// }
+
+// export default Home
+
+
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [shows, setShows] = useState([]);
 
+  useEffect(() => {
+    fetch("https://api.tvmaze.com/shows")
+      .then((response) => response.json())
+      .then((data) => setShows(data));
+  }, []);
 
-    return(
-        <p></p>
-    )
-}
+  return (
+    <div>
+      <h2>All TV Shows</h2>
+      <ul>
+        {shows.map((show) => (
+          <li key={show.id}>
+            <img src={show.image?.medium} alt={show.name} />
+            <h3>{show.name}</h3>
+            <p>{show.summary}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
