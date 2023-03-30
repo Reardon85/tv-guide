3229d95507498f1e9e4c66c4d70494b0dd68ad55
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-03-29 22:18:36 -0400

    added login request to comments

--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from "react";
-import { Route, Routes, Param} from "react-router-dom";
-import { Button, Form, Header, Comment } from 'semantic-ui-react'
+import React, { useEffect, useState } from "react";
+import { Route, Routes, Param, Link } from "react-router-dom";
+import { Button, Form, Header, Comment, Message } from 'semantic-ui-react'
 import Post from "./Post";
 import 'semantic-ui-css/semantic.min.css'
 
 
-const Discussion = ({id, user}) => {
+const Discussion = ({ id, user }) => {
 
-   
+
     const [comments, setComments] = useState([])
     const [commentsExist, setCommentsExist] = useState(true)
     const [formData, setFormData] = useState({
@@ -17,22 +17,23 @@ const Discussion = ({id, user}) => {
     })
 
 
-    useEffect (() => {
+    useEffect(() => {
 
-        if(commentsExist){
-        fetch(`http://localhost:3000/discussion/${id}`)
-        .then(r => r.json())
-        .then(d => { 
-            console.log(d)
-            if(d.id== null){
-                setCommentsExist(false)
-            }else{
-            setComments(d.comments)}
-        })
-        }else{
+        if (commentsExist) {
+            fetch(`http://localhost:3000/discussion/${id}`)
+                .then(r => r.json())
+                .then(d => {
+                    console.log(d)
+                    if (d.id == null) {
+                        setCommentsExist(false)
+                    } else {
+                        setComments(d.comments)
+                    }
+                })
+        } else {
             const commentObj = {
                 id: id,
-                comments:[]
+                comments: []
             }
 
             fetch(`http://localhost:3000/discussion/`, {
@@ -42,8 +43,8 @@ const Discussion = ({id, user}) => {
                 },
                 body: JSON.stringify(commentObj)
             })
-            .then(r => r.json())
-            .then(d => console.log("test"))
+                .then(r => r.json())
+                .then(d => console.log("test"))
         }
 
     }, [commentsExist])
@@ -65,7 +66,7 @@ const Discussion = ({id, user}) => {
         const date = new Date().toLocaleString()
         console.log(date)
 
-        
+
 
         //setComments([...comments, formData])
 
@@ -75,13 +76,13 @@ const Discussion = ({id, user}) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                comments: [...comments, {...formData, date: date}]
+                comments: [...comments, { ...formData, date: date }]
             })
         })
-        .then(r => r.json())
-        .then(d => setComments(d.comments))
+            .then(r => r.json())
+            .then(d => setComments(d.comments))
 
-        setFormData((formData) => ({...formData, text:""}))
+        setFormData((formData) => ({ ...formData, text: "" }))
 
     }
 
@@ -90,37 +91,39 @@ const Discussion = ({id, user}) => {
         return <Post key={comment.id} user={comment.user} avatar={comment.avatar} text={comment.text} date={comment.date} />
     })
 
-    
 
 
-    return(
-        
-        
+
+    return (
+
+
         <div>
-      <br/>
-    <Comment.Group>
-        <Header as='h3' dividing>
-            Comments
-        </Header>
-            {commentArray}
             <br />
-   
-    
-            
-            <Form onSubmit={handleSubmit}>
-            <Form.TextArea name="text" value={formData.text} onChange={handleChange} />
-            <Button content='Add Comment' labelPosition='left' icon='edit' primary />
-                {/* <input type="text" name="title" value={formData.title} onChange={handleChange} />
-                <input type="text" name="body" value={formData.body} onChange={handleChange} />
-                <input type="submit" /> */}
+            <Comment.Group>
+                <Header as='h3' dividing>
+                    Comments
+                </Header>
+                {commentArray}
+                <br />
 
 
-            </Form>
-        </Comment.Group>
-            
+                { user.loggedIn ? 
+                    <Form onSubmit={handleSubmit}>
+                        <Form.TextArea name="text" value={formData.text} onChange={handleChange} />
+                        <Button content='Add Comment' labelPosition='left' icon='edit' primary />
+                    </Form>
+                    :
+                    <Message>
+                    <h3>You Must Log In To Comment</h3>
+                    <Link to={'/login'}>LOG IN</Link>
+                </Message>
+
+                }
+            </Comment.Group>
+
         </div>
-       
-        
+
+
     )
 }
 
